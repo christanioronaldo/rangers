3d591dacb3031b8fc81fa9221a9b27417737b504
christanioronaldo <97797996+christanioronaldo@users.noreply.github.com>
2023-05-06 21:41:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+
+// Create canvas variable
+mycanvas="myCanvas"
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
+	var block_image_object="backg.jpg,br.png,gr.png,power-rangers-title.png,pr.png,prback.jpg,rr.jpg,rr1.png,yr.png"
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		document.getElementById("key_arrow").innerHTML = block_image_object
+		// upload red ranger
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		// upload green ranger
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	// upload blue ranger
+	}
+	
+}
+
